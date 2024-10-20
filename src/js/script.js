49af8c6409384d318cce2e526a518a40d71cd25a
syedzaquan <syedzaquan@gmail.com>
2024-10-21 01:42:45 +0800

    Ipad Air responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
+    var countDownDate = new Date("Oct 26, 2024 10:00:00").getTime();
+
+    setInterval(function () {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        $('.days').text(days);
+        $('.hours').text(hours);
+        $('.minutes').text(minutes);
+        $('.seconds').text(seconds);
+
+        if (distance < 0) {
+            clearInterval(this);
+            $('.days, .hours, .minutes, .seconds').text('00');
+        }
+    }, 1000);
+
     const textArray = ['Government Job Vacancy', 'Kerja Kosong Kerajaan'];
     let index = 0;
 
@@ -10,7 +32,7 @@ $(document).ready(function () {
             $('.work-ads-text').text(textArray[index]).removeClass('hide');
         }, 1000);
     }, 6000);
-    
+
     $('.index-1 .job-slider, .index-2 .job-slider').slick({
         infinite: true,
         slidesToShow: 1,
@@ -28,7 +50,7 @@ $(document).ready(function () {
         $('.nav-arrow.next').addClass('disabled');
         $('.nav-arrow.prev').removeClass('disabled');
     });
-    
+
     $(".nav-arrow.prev").click(function () {
         $(".index-2").fadeOut();
         $(".index-1").delay(500).fadeIn();
